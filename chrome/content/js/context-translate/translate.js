b1543c15ec8159e32476b64f4a698f10cb56ceca
Tito <tito.bouzout@gmail.com>
2013-08-25 04:14:07 -0300

    New Google URLs on translate, bug: default translating to Spanish instead of chosen language

--- chrome/content/js/context-translate/translate.js
+++ chrome/content/js/context-translate/translate.js
@@ -24,13 +24,13 @@
 						aString = this.focusedURL;
 
 					if(aString != '' && aString.indexOf('http') !== 0)
-						this.tabOpenCheckForBehavior('http://translate.google.com/translate_t',
+						this.tabOpenCheckForBehavior('http://translate.google.com/',
 												aEvent,
 												'translate',
 												'text='+this.encodeUTF8(aString.replace(/_/g, ' ').replace(/\//g, ' / ').replace(/World\//g, ' '))+'&langpair=auto|'+aEvent.originalTarget.getAttribute('value')+'&ie=UTF-8&oe=UTF-8'
 						);
 					else if(aString.indexOf('http') === 0) //translate the page
-						this.tabOpenCheckForBehavior('http://translate.google.com/translate?hl=en&sl=auto&tl=en&u='+this.encodeUTF8(aString)+'&sandbox=0',
+						this.tabOpenCheckForBehavior('http://translate.google.com/translate?sl=auto&tl='+aEvent.originalTarget.getAttribute('value')+'&js=n&prev=_t&hl=en&ie=UTF-8&u='+this.encodeUTF8(aString),
 												aEvent,
 												'translate'
 						);

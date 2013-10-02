a42cc5663c97bbb3f22052c198eebc87c95b7087
Tito <tito.bouzout@gmail.com>
2013-10-02 19:07:43 -0300

    Remove nicknames in forums

--- chrome/core/preferences.js
+++ chrome/core/preferences.js
@@ -108,9 +108,6 @@
 		'ui.context.menu.from.editor',
 		'ui.context.menu.from.editors',
 
-		//forums
-		'ui.forum.pages.custom.nicknames',
-
 		//informative panel - DONE
 		'ui.informative.panel',
 		'ui.informative.panel.closed',

a609b51c6ab43a9ccc640b79c97eb620d2962c4c
Tito <tito.bouzout@gmail.com>
2013-10-04 23:08:14 -0300

    Do not save empty values in autocomplete

--- chrome/lib/dom.js
+++ chrome/lib/dom.js
@@ -279,7 +279,9 @@
 	}
 	this.saveAutocomplete = function(aTextbox) {
 		try {
-			Components.classes["@mozilla.org/satchel/form-history;1"].getService(Components.interfaces.nsIFormHistory2).addEntry(aTextbox.getAttribute('autocompletesearchparam'), aTextbox.value);
+			var aValue = this.trim(aTextbox.value);
+			if(aValue != '')
+				Components.classes["@mozilla.org/satchel/form-history;1"].getService(Components.interfaces.nsIFormHistory2).addEntry(aTextbox.getAttribute('autocompletesearchparam'), aTextbox.value);
 		} catch (e) {}
 	}
 	//this selects nodes ala "jquery" using querySelectorAll on any HTML string.

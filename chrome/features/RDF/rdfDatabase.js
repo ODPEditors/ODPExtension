907ce49b439810e6783ca82b194fe16d02dc8b31
Tito <tito.bouzout@gmail.com>
2013-10-07 07:01:22 -0200

    Update database connection logic

--- chrome/features/RDF/rdfDatabase.js
+++ chrome/features/RDF/rdfDatabase.js
@@ -13,12 +13,15 @@
 			database.executeSimple('PRAGMA temp_store = 2');
 			database.executeSimple('PRAGMA journal_mode = memory');
 			//database.executeSimple('PRAGMA synchronous = 0');  does not free mem.
+			database.exists = database.tableExists('uris');
 		}
 		return database;
 	}
 	this.rdfDatabaseClose = function() {
-		database.close();
-		database = false;
+		if (database) {
+			database.close();
+			database = false;
+		}
 	}
 	return null;
 

fc674ce46f530769f4565c10b1790799a3da2aef
Tito <tito.bouzout@gmail.com>
2013-10-13 04:05:52 -0200

    Avoid duplicated categories in the category browser feature

--- chrome/features/category-browser/onpopupshowing.js
+++ chrome/features/category-browser/onpopupshowing.js
@@ -3,8 +3,11 @@
 	this.addListener('preferencesLoadGlobal', function() {
 
 		//session categories, categories that has been opened in this browser session
-		ODPExtension.shared.categories.session = {};
-		ODPExtension.shared.categories.session.categories = [];
+		if (!ODPExtension.shared.categories.session) {
+			ODPExtension.shared.categories.session = {};
+			ODPExtension.shared.categories.session.categories = [];
+			ODPExtension.shared.categories.session.categoriesClipboard = [];
+		}
 	});
 
 	this.addListener('focusedCategoryChange', function(aCategory) {
@@ -17,38 +20,25 @@
 		if (aEvent.originalTarget != aEvent.currentTarget)
 			return
 
-		//filling opened categories looking in all the tabs of all the windows
-
-		this.removeChildsWithAttribute(this.getElement('category-browser'), 'temporal', 'true');
-
-		var aTemp = this.windowsGetURLs();
+		var unique = []
 		var aCategories = [];
 		var aCategory = '';
 
-		for (var id in aTemp) {
-			if ((aCategory = this.categoryGetFromURL(aTemp[id])) != '') {
-				aCategories[aCategories.length] = aCategory;
-			}
-		}
-		aCategories = this.arrayUnique(aCategories).sort(this.sortLocale).reverse();
-
-		if (aCategories.length > 0) {
-			this.categoryBrowserAppendCategoriesAfter(aCategories, this.getElement('category-browser-categories-opened'), true);
-			this.getElement('category-browser-categories-opened').setAttribute('hidden', false);
-		} else {
-			this.getElement('category-browser-categories-opened').setAttribute('hidden', true);
-		}
+		this.removeChildsWithAttribute(this.getElement('category-browser'), 'temporal', 'true');
 
 		//categories from the clipboard
-
-		aTemp = (this.getClipboard().replace(/\r/g, '\n') + '\n').split('\n');
-		aCategories = [];
+		var aTemp = (this.getClipboard().replace(/\r/g, '\n') + '\n').split('\n');
 		for (var id in aTemp) {
 			if ((aCategory = this.categoryGetFromURL(aTemp[id])) != '') {
-				aCategories[aCategories.length] = aCategory;
+				if(!unique[aCategory]){
+					unique[aCategory] = true;
+					ODPExtension.shared.categories.session.categoriesClipboard[ODPExtension.shared.categories.session.categoriesClipboard.length] = aCategory;
+					if(ODPExtension.shared.categories.session.categoriesClipboard.length > 12)
+						ODPExtension.shared.categories.session.categoriesClipboard.shift();
+				}
 			}
 		}
-		aCategories = this.arrayUnique(aCategories).sort(this.sortLocale).reverse();
+		aCategories = this.arrayUnique(ODPExtension.shared.categories.session.categoriesClipboard).sort(this.sortLocale).reverse();
 		if (aCategories.length > 0) {
 			this.categoryBrowserAppendCategoriesAfter(aCategories, this.getElement('category-browser-categories-clipboard'), true);
 			this.getElement('category-browser-categories-clipboard').setAttribute('hidden', false);
@@ -56,12 +46,38 @@
 			this.getElement('category-browser-categories-clipboard').setAttribute('hidden', true);
 		}
 
-		//categories for this session
+		//filling opened categories looking in all the tabs of all the windows
+		aCategories = [];
+		var aTemp = this.windowsGetURLs();
+		for (var id in aTemp) {
+			if ((aCategory = this.categoryGetFromURL(aTemp[id])) != '') {
+				if(!unique[aCategory]){
+					unique[aCategory] = true;
+					aCategories[aCategories.length] = aCategory;
+				}
+			}
+		}
+		aCategories = this.arrayUnique(aCategories).sort(this.sortLocale).reverse();
+		if (aCategories.length > 0) {
+			this.categoryBrowserAppendCategoriesAfter(aCategories, this.getElement('category-browser-categories-opened'), true);
+			this.getElement('category-browser-categories-opened').setAttribute('hidden', false);
+		} else {
+			this.getElement('category-browser-categories-opened').setAttribute('hidden', true);
+		}
 
-		if (this.shared.categories.session.categories.length > 0) {
+		//categories for this session
+		aCategories = [];
+		for (var id in this.shared.categories.session.categories) {
+			aCategory = this.shared.categories.session.categories[id]
+			if(!unique[aCategory]){
+				unique[aCategory] = true;
+				aCategories[aCategories.length] = aCategory;
+			}
+		}
+		if (aCategories > 0) {
 			this.removeChilds(this.getElement('category-browser-session-categories'));
 			this.getElement('category-browser-session-categories').parentNode.setAttribute('disabled', false);
-			this.categoryBrowserAppendCategoriesAfter(this.shared.categories.session.categories.sort(this.sortLocale), this.getElement('category-browser-session-categories').firstChild, true);
+			this.categoryBrowserAppendCategoriesAfter(aCategories.sort(this.sortLocale), this.getElement('category-browser-session-categories').firstChild, true);
 		} else
 			this.getElement('category-browser-session-categories').parentNode.setAttribute('disabled', true);
 

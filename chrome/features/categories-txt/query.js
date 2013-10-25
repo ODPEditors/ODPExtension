55c8319ebae55cd79a04bfe8050574a7f0e91a1b
Tito <tito.bouzout@gmail.com>
2013-10-25 08:54:47 -0200

    Fix bug when searching without restricting to a category

--- chrome/features/categories-txt/query.js
+++ chrome/features/categories-txt/query.js
@@ -4,8 +4,10 @@
 	this.addListener('databaseReady', function() {
 
 		db = ODPExtension.categoriesTXTDatabaseOpen();
-		if (db.exists)
+		if (db.exists){
 			query = db.query('select * from categories_txt where category GLOB :category');
+			query_all = db.query('select * from categories_txt');
+		}
 
 	});
 	//searchs in the categories.txt database
@@ -17,13 +19,18 @@
 		results.categories = [];
 
 		if (!db.exists) {} else {
-
-			query.params('category', aWhere.replace(/\/$/, '') + '*');
+			if(aWhere == ''){
+				this.notifyOnce('Warning: Searching for categories without restricting the search to a branch is super slow! (a minute~ or so)');
+				var fetch_from = query_all;
+			} else {
+				query.params('category', aWhere.replace(/\/$/, '') + '*');
+				var fetch_from = query;
+			}
 
 			if (!aSearchEngineSearch) {
 				aQuery = this.trim(aQuery).replace(/ /g, '_').replace(/\/*\$$/g, '/$');
 				var row;
-				for (var i = 0; row = db.fetchObjects(query); i++) {
+				for (var i = 0; row = db.fetchObjects(fetch_from); i++) {
 					if (aQuery == '' || this.match(row.category, aQuery)) {
 						results.categories[results.count] = row.category;
 						results.count++;
@@ -32,7 +39,7 @@
 			} else {
 				aQuery = this.trim(aQuery).replace(/_/g, ' ');
 				var row;
-				for (var i = 0; row = db.fetchObjects(query); i++) {
+				for (var i = 0; row = db.fetchObjects(fetch_from); i++) {
 					if (aQuery == '' || this.searchEngineSearch(aQuery, row.category.replace(/_/g, ' ').replace(/-/g, ' '))) {
 						results.categories[results.count] = row.category;
 						results.count++;

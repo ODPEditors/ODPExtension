f55142db72a2ea58cf86474181f4f8191346df68
Tito Bouzout <tito.bouzout@gmail.com>
2014-03-13 09:15:13 -0300

    Prevent database corruption

--- chrome/features/SQLiteRAW/database.js
+++ chrome/features/SQLiteRAW/database.js
@@ -13,8 +13,12 @@
 			database.executeSimple('PRAGMA temp_store = 2');
 			database.executeSimple('PRAGMA secure_delete = false');
 			database.executeSimple('PRAGMA read_uncommitted = true');
-			database.executeSimple('PRAGMA journal_mode = memory');//memory
-			//database.executeSimple('PRAGMA synchronous = 0');  does not free mem.
+
+			//The MEMORY journaling mode stores the rollback journal in volatile RAM. This saves disk I/O but at the expense of database safety and integrity. If the application using SQLite crashes in the middle of a transaction when the MEMORY journaling mode is set, then the database file will very likely go corrupt.
+			//database.executeSimple('PRAGMA journal_mode = memory');
+
+			//When synchronous is FULL (2), the SQLite database engine will use the xSync method of the VFS to ensure that all content is safely written to the disk surface prior to continuing. This ensures that an operating system crash or power failure will not corrupt the database.
+			database.executeSimple('PRAGMA synchronous = OFF');
 		}
 		return database;
 	}

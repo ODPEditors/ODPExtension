780ddee7d4151b0c5b94d98fdaad272e937bf59d
Tito Bouzout <tito.bouzout@gmail.com>
2014-03-18 11:26:35 -0300

    Default to the URL

--- chrome/features/context-translate/translate.js
+++ chrome/features/context-translate/translate.js
@@ -6,34 +6,31 @@
 
 	//translate the selected text or ask the user for input
 	//it also replaces the "_" for " " nice for translate category names
+
 	this.translate = function(aEvent) {
+
 		//fix bug for when the menuitem were moved, don't no why
 		this.stopEvent(aEvent);
 
+		var aURL = this.getSelectedLinkURL();
+		if(!aURL)
+			aURL = this.focusedURL
+
 		//the selected text
-		var aString = this.getSelectedTextOrPrompt(false);
+		var aString = this.getSelectedTextOrPrompt(false, aURL);
 
 		if (aString != '' && aString.indexOf('http') !== 0) {
 			this.tabOpenCheckForBehavior('https://translate.google.com/',
 				aEvent,
 				'translate',
-				'text=' + this.encodeUTF8(aString.replace(/_/g, ' ').replace(/\//g, ' / ').replace(/World\//g, ' ')) + '&langpair=auto|' + aEvent.originalTarget.getAttribute('value') + '&ie=UTF-8&oe=UTF-8');
+				'text=' + this.encodeUTF8(aString.replace(/World\//g, ' ').replace(/_/g, ' ').replace(/\//g, ' / ')) + '&langpair=auto|' + aEvent.originalTarget.getAttribute('value') + '&ie=UTF-8&oe=UTF-8');
 		} else {
 
-			//the linked url
-			if (aString == '')
-				aString = this.getSelectedLinkURL();
-
-			//the focused url
-			if (aString == '') {
-				if (!this.prompt('Translate focused URL!?', this.focusedURL))
-					return;
-				aString = this.focusedURL;
-			}
-			if (aString != '')
+			if (aString.indexOf('http') === 0)
 				this.tabOpenCheckForBehavior('https://translate.google.com/translate?sl=auto&tl=' + aEvent.originalTarget.getAttribute('value') + '&js=n&prev=_t&hl=en&ie=UTF-8&u=' + this.encodeUTF8(aString),
 					aEvent,
 					'translate');
+
 		}
 	}
 	return null;

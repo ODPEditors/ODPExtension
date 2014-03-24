a69a8ab6695e3fe05f6244e901a5d28c1b7bbf5d
Tito Bouzout <tito.bouzout@gmail.com>
2014-03-24 15:27:24 -0300

    Kill the workers

--- chrome/lib/workers.js
+++ chrome/lib/workers.js
@@ -9,21 +9,26 @@
 		listeners[aEvent.data.id] = null
 		//workers[aEvent.data.idWorker].terminate();
 		workers[aEvent.data.idWorker] = null
-	};
+	}
 
-	this.worker = function(aURL, aMessage, aFunction){
+	this.worker = function(aURL, aMessage, aFunction) {
 		//move of thread
 		thread++
-		var _thread = String(thread)
+		var idWorker = String(thread)
 		//save the listener
 		listeners.push(aFunction)
 		var id = listeners.length - 1;
-		aMessage.idWorker = _thread
+		aMessage.idWorker = idWorker
 		aMessage.id = id
 		//spawn the worker
-		workers[_thread] = new ChromeWorker(aURL);
-		workers[_thread].onmessage = workerOnMessage
-		workers[_thread].postMessage(aMessage)
+		workers[idWorker] = new ChromeWorker(aURL);
+		workers[idWorker].onmessage = workerOnMessage
+		workers[idWorker].onerror = function(aError){
+			listeners[id] = null
+			workers[idWorker].terminate();
+			workers[idWorker] = null
+		}
+		workers[idWorker].postMessage(aMessage)
 	}
 
 	return null;

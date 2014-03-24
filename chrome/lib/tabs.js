9d20ba1cec458f9f7fb6a2e28e5a70501d6f49cc
Tito Bouzout <tito.bouzout@gmail.com>
2014-03-24 15:26:58 -0300

    Not need for a try and catch

--- chrome/lib/tabs.js
+++ chrome/lib/tabs.js
@@ -2,11 +2,10 @@
 	// close a tab and return true if the tab was closed. It will return false if the tab is protected by another extension
 	this.tabClose = function(aTab) {
 		if (!aTab.hasAttribute('isPermaTab') && !aTab.hasAttribute('protected')) {
-			try {
+			if(!gBrowser.removeTab)
 				gBrowser.closeTab(aTab);
-			} catch (e) {
+			else
 				gBrowser.removeTab(aTab);
-			}
 			return true;
 		}
 		return false;

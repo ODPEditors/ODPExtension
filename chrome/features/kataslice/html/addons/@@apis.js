dea86fdd28f5fb46d911aef171a3dc6c97ae6de3
Tito Bouzout <tito.bouzout@gmail.com>
2014-04-21 19:25:07 -0300

    Fix api loading

--- chrome/features/kataslice/html/addons/@@apis.js
+++ chrome/features/kataslice/html/addons/@@apis.js
@@ -4,60 +4,59 @@
 
 */
 
-var api ={}
-
+var api = {}
+add('onLoad', function(){
+	giveAPIs(window)
+});
 
 function giveAPIs(source) {
-	source.api = api
-}
 
-add('onLoad', function(){
 
 	//escapeHTML
-	api.h = function (aText) {
+	source.api.h = function (aText) {
 		return ODP.h(aText);
 	}
 
 	//super basic
-	api.setTimeout = function (aFunction, aTime) {
+	source.api.setTimeout = function (aFunction, aTime) {
 		return setTimeout(aFunction, aTime);
 	};
-	api.setInterval = function (aFunction, aTime) {
+	source.api.setInterval = function (aFunction, aTime) {
 		return setInterval(aFunction, aTime);
 	};
-	api.clearTimeout = function (aID) {
+	source.api.clearTimeout = function (aID) {
 		clearTimeout(aID);
 	};
-	api.clearInterval = function (aID) {
+	source.api.clearInterval = function (aID) {
 		clearInterval(aID);
 	};
 
 	//almost super basic
-	api.alert = function (aString) {
+	source.api.alert = function (aString) {
 		ODP.alert(aString);
 	};
-	api.prompt = function (aString, aDefault) {
+	source.api.prompt = function (aString, aDefault) {
 		return ODP.prompt(aString, aDefault);
 	};
-	api.confirm = function (aString) {
+	source.api.confirm = function (aString) {
 		return ODP.confirm(aString);
 	};
 
 	//basic
-	api.console = console;
-	api.l = console.log;
-	api.dump = function (aData) {
+	source.api.console = console;
+	source.api.l = console.log;
+	source.api.dump = function (aData) {
 		ODP.dump(aData);
 	};
-	api.arrayUnique = function (array) {
+	source.api.arrayUnique = function (array) {
 		return ODP.arrayUnique(array);
 	}
 
 	//browser related
-	api.tabOpen = function (aURLs, selected, aPostData, inNoTree) {
+	source.api.tabOpen = function (aURLs, selected, aPostData, inNoTree) {
 		return ODP.tabOpen(aURLs, selected, aPostData, inNoTree);
 	}
-	api.tabOpenEditable = function (which) {
+	source.api.tabOpenEditable = function (which) {
 		var uris = []
 		var sites = []
 		which.forEach(function (d) {
@@ -66,35 +65,35 @@ add('onLoad', function(){
 		});
 		ODP.panelFastAddOpenTabsWithLinkedPanel(uris, sites, true);
 	}
-	api.tabClose = function (aTab) {
+	source.api.tabClose = function (aTab) {
 		return ODP.tabClose(aTab);
 	}
-	api.tabSelect = function (aTab) {
+	source.api.tabSelect = function (aTab) {
 		ODP.tabSelect(aTab);
 	}
 
 	//utilities, template engine
-	api.html = function (html) {
+	source.api.html = function (html) {
 		return _.template(html);
 	}
 
 	//slice related
-	api.sitesGetVisible = sitesGetVisible;
-	api.sitesGetSelected = sitesGetSelected;
-	api.sitesGetModified = sitesGetModified;
-	api.sitesGetAll = sitesGetAll;
+	source.api.sitesGetVisible = sitesGetVisible;
+	source.api.sitesGetSelected = sitesGetSelected;
+	source.api.sitesGetModified = sitesGetModified;
+	source.api.sitesGetAll = sitesGetAll;
 
-	api.sitesSelectAll = sitesSelectAll;
-	api.sitesSelectNone = sitesSelectNone;
-	api.sitesSelectInvert = sitesSelectInvert;
+	source.api.sitesSelectAll = sitesSelectAll;
+	source.api.sitesSelectNone = sitesSelectNone;
+	source.api.sitesSelectInvert = sitesSelectInvert;
 
-	api.sitesCount = function(){ return total_count; }
-	api.sitesCountVisible = function(){ return total_visible; }
-	api.sitesCountModified = function(){ return total_modified; }
-	api.sitesCountSelected = function(){ return total_selected; }
+	source.api.sitesCount = function(){ return total_count; }
+	source.api.sitesCountVisible = function(){ return total_visible; }
+	source.api.sitesCountModified = function(){ return total_modified; }
+	source.api.sitesCountSelected = function(){ return total_selected; }
 
 	//sub tools
-	api.linkChecker = function () {
+	source.api.linkChecker = function () {
 
 		var oRedirectionAlert = ODP.redirectionAlert();
 		return {
@@ -107,16 +106,15 @@ add('onLoad', function(){
 	}
 
 	//a reference to other addons
-	api.addon = addon;
+	source.api.addon = addon;
 
 	//odp related
-	api.promptForCategory = function (aFunction) {
+	source.api.promptForCategory = function (aFunction) {
 		ODP.promptForCategory(aFunction);
 	};
-	api.categoryGetURLEdit = function (aURL) {
+	source.api.categoryGetURLEdit = function (aURL) {
 		return ODP.categoryGetURLEdit(aURL);
 	}
 
-	api.ODP = ODP;
-
-});
+	source.api.ODP = ODP;
+}
